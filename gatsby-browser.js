c69c46acf3e62334267104ac281ccdbab6dea0e4
Rabii Luena <itisrabii@gmail.com>
2020-08-10 11:57:33 +0300

    chore: adding exprimental fonts

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,4 +4,8 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
+import "typeface-advent-pro";
+import "typeface-great-vibes";
+import "typeface-fredoka-one";
+
 // You can delete this file if you're not using it
