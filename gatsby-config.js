5541502ef677e7fae5a95d7c789f2ca835961224
Rabii Luena <itisrabii@gmail.com>
2020-08-11 03:43:03 +0300

    chore: updating images plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,21 @@
 module.exports = {
   siteMetadata: {
     title: `Rabii Luena`,
-    headline: `Software Engineer and JavaScript Enthusiast`,
-    description: `Hi! I am Rabii Luena, a front-end and mobile application developer. `,
+    headline: `Front-end Engineer and JavaScript Enthusiast`,
+    description: `Hi! I am Rabii Luena, a front-end and mobile application developer.`,
     author: `@rbluena`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
     {
       resolve: `gatsby-plugin-styled-components`,
       options: {
