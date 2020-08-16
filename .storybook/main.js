e3f67be745d02ddd287fd0c97665f2cb5d2f656d
Rabii Luena <itisrabii@gmail.com>
2020-08-16 15:35:41 +0300

    chore: adding storybook addons

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,9 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
-}
+    "@storybook/addon-storysource",
+    "@storybook/addon-addon-a11y",
+    "@storybook/addon-essentials",
+  ],
+};
