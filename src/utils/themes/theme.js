b94d465ff2fce53d7284db1a23a9ba86e168332b
Rabii Luena <itisrabii@gmail.com>
2020-08-18 15:31:41 +0300

    feat: adding design tokens for design system

--- src/utils/themes/theme.js
+++ src/utils/themes/theme.js
@@ -1,9 +1,9 @@
+import { typography, spacing, border } from "./tokens.json";
+
 export const defaultTheme = {
   colors: {
-    bg: {
-      primary: "#FFFFFF",
-    },
-    foreColors: {},
+    primary: "#63C7FF",
+    secondary: "#FFFFFF",
     monochrome: {
       lightest: "#FFFFFF",
       lighter: "#F8F8F8",
@@ -16,38 +16,9 @@ export const defaultTheme = {
       darkest: "#333333",
     },
   },
-  typography: {
-    type: {
-      primary: '"Lato", "Helvetica Neue", Arial, sans-serif;',
-    },
-    size: {
-      xs: "12",
-      sm: "14",
-      base: "16",
-      large: "18",
-      h6: "20",
-      h5: "24",
-      h4: "36",
-      h3: "42",
-      h2: "48",
-      h1: "64",
-    },
-    weight: {
-      thin: "300",
-      regular: "400",
-      medium: "700",
-      bold: "800",
-      xbold: "900",
-    },
-  },
-  spacing: {
-    padding: {
-      small: 10,
-      medium: 20,
-      large: 30,
-    },
-    // padding: "0 1.0875rem 1.45rem",
-  },
+  typography: { ...typography },
+  spacing: { ...spacing },
+  border: { ...border },
 };
 
 export const invertTheme = {
@@ -57,9 +28,8 @@ export const invertTheme = {
   colors: {
     primary: "#FFFFFF",
   },
-  typography: {
-    primary: '"Lato", Helvetic, Arial, sans-serif;',
-  },
+  typography: { ...typography },
+  spacing: { ...spacing },
 };
 
 export const laxuryTheme = {};
