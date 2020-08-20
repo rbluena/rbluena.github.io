687d1f7a6d13de6f489e34cadb049c60e6f4a5a8
Rabii Luena <itisrabii@gmail.com>
2020-08-20 12:59:20 +0300

    fix: __PATH_PREFIX__ issue with gatsby and storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -20,3 +20,23 @@ export const decorators = [
     </ThemeProvider>
   ),
 ];
+
+// All down here trying to fix isseues with storybook and Gatsby
+
+// Gatsby internal mocking to prevent unnecessary errors in storybook testing environment
+global.__PATH_PREFIX__ = "";
+global.__BASE_PATH__ = "";
+
+// Gatsby's Link overrides:
+// Gatsby Link calls the `enqueue` & `hovering` methods on the global variable ___loader.
+// This global object isn't set in storybook context, requiring you to override it to empty functions (no-op),
+// so Gatsby Link doesn't throw any errors.
+global.___loader = {
+  enqueue: () => {},
+  hovering: () => {},
+};
+// Navigating through a gatsby app using gatsby-link or any other gatsby component will use the `___navigate` method.
+// In Storybook it makes more sense to log an action than doing an actual navigate. Checkout the actions addon docs for more info: https://github.com/storybookjs/storybook/tree/master/addons/actions.
+window.___navigate = pathname => {
+  action("NavigateTo:")(pathname);
+};
