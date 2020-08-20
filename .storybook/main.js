12afc61481cd0115e5de7e7ac107e4e2087f6eca
Rabii Luena <itisrabii@gmail.com>
2020-08-20 12:12:26 +0300

    fix: importing of Link component from gatsby in storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 module.exports = {
   stories: [
     "../src/components/**/*.stories.mdx",
@@ -9,4 +11,25 @@ module.exports = {
     "@storybook/addon-a11y",
     "@storybook/addon-essentials",
   ],
+
+  // Webpack Configurations
+  webpackFinal: async config => {
+    config.resolve.modules = [
+      path.resolve(__dirname, "..", "src"),
+      "node_modules",
+    ];
+
+    // Alternately, for an alias:
+    config.resolve.alias = {
+      "@app-components": path.resolve(__dirname, "..", "src", "components"),
+      "@app-data": path.resolve(__dirname, "..", "src", "data"),
+    };
+
+    // Transpile Gatsby module because Gatsby includes un-transpiled ES6 code.
+    config.module.rules[0].exclude = [/node_modules\/(?!(gatsby)\/)/];
+    // use installed babel-loader which is v8.0-beta (which is meant to work with @babel/core@7)
+    config.module.rules[0].use[0].loader = require.resolve("babel-loader");
+
+    return config;
+  },
 };
