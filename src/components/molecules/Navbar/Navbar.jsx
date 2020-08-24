d608614e591e2c676f7b2536329bfda0a270d49e
Rabii Luena <itisrabii@gmail.com>
2020-08-24 09:32:02 +0300

    feat: adding illustration

--- src/components/molecules/Navbar/Navbar.jsx
+++ src/components/molecules/Navbar/Navbar.jsx
@@ -9,35 +9,6 @@ import { FiMenu } from "react-icons/fi";
 import { motion } from "framer-motion";
 import animation from "./animation";
 
-const StyledNavOld = styled.nav`
-  display: flex;
-  width: 100%;
-  right: 0;
-  position: absolute;
-  line-height: 10px;
-
-  text-transform: uppercase;
-  top: 0px;
-  justify-content: flex-end;
-
-  a {
-    display: block;
-    padding-left: ${props => props.theme.spacing.sm}px;
-
-    &:hover,
-    &:active {
-      background-color: ${props => props.theme.color.monochrome[700]};
-    }
-  }
-
-  @media screen and (max-width: ${props => props.theme.mediaQuery.tablet}) {
-    flex-direction: column;
-    top: ${props => props.theme.spacing.xxxl}px;
-    box-shadow: ${props => props.theme.elevation.elevated};
-    background-color: white;
-  }
-`;
-
 const StyledNavbar = styled.nav`
   position: absolute;
   display: flex;
@@ -97,8 +68,7 @@ const Menu = styled(motion.div)`
 `;
 
 const MobMenuHeading = styled.div`
-  display: none;
-
+  /* Tablet */
   @media screen and (min-width: ${props => props.theme.mediaQuery.tablet}) {
     display: none;
   }
--- src/components/organisms/Header/Header.jsx
+++ src/components/organisms/Header/Header.jsx
@@ -2,15 +2,17 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import Navbar from "@app-components/molecules/Navbar";
-
 import CoverBG from "@app-components/assets/CoverBG";
+import Illustration from "@app-components/assets/Illustration";
 import PersonalDetails from "./PersonalDetails";
-import { FiMenu } from "react-icons/fi";
-// import SvgIllustration from "@app-components/assets/SvgIllustration";
 
 const Wrapper = styled.header`
-  margin: 0;
-  padding: 0;
+  margin-top: ${props => props.theme.spacing.xxxl}px;
+
+  /* Tablet and greater */
+  @media screen and (min-width: ${props => props.theme.mediaQuery.tablet}) {
+    margin-top: ${props => props.theme.spacing.xxxxl}px;
+  }
 `;
 
 const ContentContainer = styled.div`
@@ -22,12 +24,35 @@ const ContentContainer = styled.div`
     position: absolute;
     top: 0;
     left: 0;
+    fill: ${props => props.theme.color.blue[100]};
+    color: ${props => props.theme.color.blue[100]};
   }
 
   svg#illustration {
     display: none;
     position: absolute;
     right: 0;
+    margin: 0p;
+    padding: 0;
+  }
+
+  /* Tablet and greater */
+  @media screen and (min-width: ${props => props.theme.mediaQuery.tablet}) {
+    width: 400px;
+    border: 1px dashed;
+    padding-top: ${props => props.theme.spacing.xxxxl}px;
+
+    svg#bg-wiggle {
+      max-width: 50vw;
+      top: -${props => props.theme.spacing.medium}px;
+      display: none;
+    }
+
+    svg#illustration {
+      display: block;
+      width: 50vw;
+      top: ${props => props.theme.spacing.xxxl}px;
+    }
   }
 `;
 
@@ -38,6 +63,7 @@ const Header = props => {
       <ContentContainer>
         <CoverBG />
         <PersonalDetails />
+        <Illustration />
       </ContentContainer>
     </Wrapper>
   );
