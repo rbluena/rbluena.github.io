06c6c91961912dbedaafce18dfd233e0caab4254
Rabii Luena <itisrabii@gmail.com>
2020-08-29 11:37:30 +0300

    refactor: importing all components from '@app-components' path

---
+++ src/components/index.js
@@ -0,0 +1,28 @@
+// Molecules components
+import SectionBox from "./molecules/SectionBox";
+import SkillBlock from "./molecules/SkillBlock";
+
+// Organism components
+import Education from "./organisms/Education";
+import Footer from "./organisms/Footer";
+import Header from "./organisms/Header";
+import Layout from "./organisms/Layout";
+import Navbar from "./organisms/Navbar";
+import SEO from "./organisms/SEO";
+import Skills from "./organisms/Skills";
+import Tags from "./organisms/Tags";
+import Work from "./organisms/Work";
+
+export {
+  SectionBox,
+  SkillBlock,
+  Education,
+  Footer,
+  Header,
+  Layout,
+  Navbar,
+  SEO,
+  Skills,
+  Tags,
+  Work,
+};
