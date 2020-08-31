88810361f173b914d6f9833345eeb571458c38da
Rabii Luena <itisrabii@gmail.com>
2020-08-31 09:57:27 +0300

    feat: creating 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,24 @@
 import React from "react";
-import { Layout, SEO } from "@app-components";
+import { Layout, Main, Section, Header, Text, SEO } from "@app-components";
+import { Footer } from "@app-components";
+import styled from "styled-components";
+
+const Heading = styled(Text).attrs({ size: "title" })`
+  margin-top: ${props => props.theme.spacing["4xl"]}px;
+  margin-bottom: ${props => props.theme.spacing["large"]}px;
+`;
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="Not found" />
+    <Header />
+    <Main>
+      <Section>
+        <Heading>NOT FOUND</Heading>
+        <Text size="xl">You just hit a route that doesn&#39;t exist.</Text>
+      </Section>
+    </Main>
+    <Footer />
   </Layout>
 );
 
