3696e2f33afdce0f691999e6ce7c5a638c51d52e
Rabii Luena <itisrabii@gmail.com>
2020-08-31 14:13:43 +0300

    feat: building home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,22 @@
 import React from "react";
-import { Layout, Header, Footer, SEO } from "@app-components";
+import {
+  Layout,
+  Header,
+  Footer,
+  SEO,
+  Work,
+  Skills,
+  Tags,
+} from "@app-components";
 
 export default () => {
   return (
     <Layout>
       <SEO title="Home" />
       <Header showHero />
+      <Tags />
+      <Skills />
+      <Work />
       <Footer />
     </Layout>
   );
