9ab46f3abdc560eca579e91cef22fa8f022d1ec7
Rabii Luena <itisrabii@gmail.com>
2020-09-06 22:20:32 +0300

    refactor: moving error component outside form control

--- src/components/atoms/InputText/InputText.jsx
+++ src/components/atoms/InputText/InputText.jsx
@@ -35,7 +35,8 @@ const Input = styled.input.attrs({ type: "text" })`
 const Error = styled.div`
   display: block;
   font-size: ${props => props.theme.typography.size.sm}px;
-  padding: ${props => props.theme.spacing.sm}px;
+  padding: ${props =>
+    `${props.theme.spacing.xs}px ${props.theme.spacing.sm}px`};
   color: ${props => props.theme.color.red[100]};
 `;
 
@@ -45,7 +46,7 @@ const Wrapper = styled.div`
   border-color: ${props => props.theme.color.monochrome[200]};
   height: 50px;
   padding: 0;
-  margin: 0px;
+  margin: 0;
 
   ${Input} {
     &:focus {
@@ -60,28 +61,30 @@ const InputText = ({ id, label, value, error, onChange, ...rest }) => {
   const errorMessage = error;
 
   return (
-    <Wrapper>
-      <Label
-        htmlFor={id}
-        focused={focused}
-        notEmpty={value && value.length > 0}
-      >
-        {label}
-      </Label>
-      <Input
-        id={id}
-        value={value}
-        onFocus={() => setFocused(true)}
-        onBlur={() => setFocused(false)}
-        onChange={onChange}
-        aria-describedby={errorId}
-        {...rest}
-      />
-
+    <>
+      <Wrapper>
+        <Label
+          htmlFor={id}
+          focused={focused}
+          notEmpty={value && value.length > 0}
+        >
+          {label}
+        </Label>
+        <Input
+          id={id}
+          name={id}
+          value={value}
+          onFocus={() => setFocused(true)}
+          onBlur={() => setFocused(false)}
+          onChange={onChange}
+          aria-describedby={errorId}
+          {...rest}
+        />
+      </Wrapper>
       {errorMessage && errorMessage.length && (
         <Error id={errorId}>{errorMessage}</Error>
       )}
-    </Wrapper>
+    </>
   );
 };
 
