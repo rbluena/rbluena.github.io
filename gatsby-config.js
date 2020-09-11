57008a5a6a568c97ffc7de1313771494aef7f125
Rabii Luena <itisrabii@gmail.com>
2020-09-11 09:49:43 +0300

    feat: adding sitemap generator

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-sitemap`,
     `gatsby-plugin-flow`,
     {
       resolve: `gatsby-source-filesystem`,
