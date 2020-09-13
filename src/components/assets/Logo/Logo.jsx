2c01d2515c5d546ad8b91e556769ad7a2e2496f4
Rabii Luena <itisrabii@gmail.com>
2020-09-13 10:55:51 +0300

    docs: updating documentation for the logo

--- src/components/assets/Logo/Logo.jsx
+++ src/components/assets/Logo/Logo.jsx
@@ -1,12 +1,21 @@
-import React from "react";
-import styled from "styled-components";
+import * as React from "react";
 
-const Wrapper = styled.div`
-  font-size: 24px;
-  text-transform: uppercase;
-  letter-spacing: 4px;
-  font-weight: ${props => props.theme.typography.weight.bold};
-  color: ${props => props.theme.color.blue[100]};
-`;
+const Logo = props => {
+  return (
+    <svg width={22} height={30} viewBox="0 0 22 30" fill="none" {...props}>
+      <circle cx={9.5} cy={7.5} r={7.5} fill="#30D2F5" />
+      <rect width={5} height={29} rx={2.5} fill="#30D2F5" />
+      <rect
+        x={0.065}
+        y={11.633}
+        width={5}
+        height={24.834}
+        rx={2.5}
+        transform="rotate(-45.578 .065 11.633)"
+        fill="#30D2F5"
+      />
+    </svg>
+  );
+};
 
-export default () => <Wrapper>Luena</Wrapper>;
+export default Logo;
