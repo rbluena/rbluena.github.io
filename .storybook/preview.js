a7452eeca6854b32b98c07af9b0ecd593138830f
Rabii Luena <itisrabii@gmail.com>
2020-09-14 08:42:31 +0300

    docs: re-ordering stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,16 +1,15 @@
 import React, { Fragment } from "react";
-import { addDecorator } from "@storybook/react";
+import sortArray from "sort-array";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle, defaultTheme } from "../src/utils/themes";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-  // options: {
-  //   storySort: (a, b) =>
-  //     a[1].kind === b[1].kind
-  //       ? 0
-  //       : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
-  // },
+  options: {
+    storySort: {
+      order: ["Introduction", "Design System", ["Design Tokens"]],
+    },
+  },
 };
 
 export const decorators = [
