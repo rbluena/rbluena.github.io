8c7d5e72c5bb15a8f91d5456cda546e0153e0c50
Rabii Luena <itisrabii@gmail.com>
2020-09-15 15:16:41 +0300

    fix: fixing padding from top in mobile view

--- src/components/molecules/Main/Main.jsx
+++ src/components/molecules/Main/Main.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 
 const Wrapper = styled.main`
   min-height: 20em;
+  padding-top: ${props => props.theme.spacing["3xl"]}px;
 `;
 
 const Main = ({ children }) => {
